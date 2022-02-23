72f17757b97706c02d6a48a77c7ffd8ad2d06111
tuvshuu1211 <tuvshuu1211@gmail.com>
2022-02-23 17:49:23 +0800

    update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'someRandomLongString'),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
   },
 });
